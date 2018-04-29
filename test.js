310acb10ca79c5949ab73ce14eb1ecee57890b21
Suguru Motegi <suguru.motegi@gmail.com>
2018-04-29 16:42:43 -0700

    update test handler

--- test.js
+++ test.js
@@ -17,6 +17,9 @@ const re = new RegExp(target);
   _.forEach(fs.readdirSync(dirpath), filename => {
     const filepath = path.resolve(dirpath, filename);
     if (fs.statSync(filepath).isDirectory()) {
+      if (!re.test(filename)) {
+        return;
+      }
       const map = { name: filename };
       resolve(filepath, map);
       createTest(map);
@@ -33,44 +36,60 @@ function createTest({ name, index, test }) {
   if (!index || !test) {
     throw new Error('index or test not found');
   }
-  const event = new Event();
-  const ctx = {
-    process,
-    console: {
-      log(...args) {
-        console.log(...args);
-        event.emit('data', args[0]);
-      },
-    },
-  };
-  const { main } = new Agent(index, ctx).run().getInnerVariable();
   const testCases = require(test);
-  let testIndex = 0;
-  let count = 0;
   const keys = _.chain(testCases)
     .first()
     .omit('result')
     .keys()
     .value();
-  const size = keys.length;
+
+  // create dummy
+  const event = new Event();
+  const log = (...args) => {
+    console.log(...args);
+    event.emit('data', args[0]);
+  };
+  const dummyFs = {
+    createWriteStream() {
+      return {
+        write: log,
+        end() {},
+      };
+    },
+  };
+  const ctx = {
+    require() {
+      return dummyFs;
+    },
+    process,
+    console: { log },
+  };
 
   describe(name, () => {
     _.forEach(testCases, test => {
       let str = _.reduce(keys, (s, key) => `${s}${key}: ${test[key]} `, '');
       str += `-> ${test.result}`;
       it(str, () => {
+        new Agent(index, ctx).run();
+
+        // run test cases
         const logs = [];
-        event.on('data', log);
+        event.on('data', data => logs.push(data));
         _.forEach(keys, key => {
           process.stdin.emit('data', `${test[key]}\n`);
         });
         process.stdin.emit('end');
-        event.removeListener('data', log);
-        assert.deepStrictEqual(logs.pop(), test.result);
 
-        function log(data) {
-          logs.push(data);
-        }
+        // remove events
+        event.removeAllListeners('data');
+        process.stdin.removeAllListeners('data');
+        process.stdin.removeAllListeners('end');
+
+        const result = logs.pop();
+        assert.deepEqual(
+          typeof result === 'string' ? result.trim() : result,
+          test.result,
+        );
       });
     });
   });
